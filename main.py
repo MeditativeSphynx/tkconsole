86ebede78d89762ddb9fa850fc0d981a79983f11
Daniel <meditativesphynx@gmail.com>
2020-07-25 15:16:48 -0400

    add mainloop

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
+from time import sleep
+
 from faker import Faker
 
 from Console import interface
+from Console import q
 
 
 if __name__ == '__main__':
-    pass
+    ui_root = interface.Root(title='Sign Ups')
+    console_frame = interface.ConsoleFrame(ui_root)
+    ui_root.mainloop()
