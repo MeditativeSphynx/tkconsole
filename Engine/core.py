27f4a8ef858389589cd4d3361e31948520fdb5f3
Daniel <meditativesphynx@gmail.com>
2020-07-27 14:03:57 -0400

    add the sim engine core

---
+++ Engine/core.py
@@ -0,0 +1,10 @@
+class Cycle:
+    def __init__(self):
+        self._is_live = True
+    
+    def update(self):
+        pass
+
+    def main_process(self):
+        while self._is_live:
+            pass
